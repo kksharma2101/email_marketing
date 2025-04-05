855cbcb9f6bde3367632334ff99377e47f2d07b4
kamal sharma <124430716+kksharma2101@users.noreply.github.com>
2025-04-05 21:41:22 +0530

    Delete server directory

--- server/src/routes/email.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendSchedualEmail } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-router.post("/schedule-email", sendSchedualEmail);
-
-export default router;
